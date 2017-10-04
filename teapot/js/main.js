4cfc071ac47b3fceac5c62a95aa9bd4f3cbac021
SethFranklin <sethfranklin@gmail.com>
2017-10-03 22:48:48 -0400

    Update main.js

--- teapot/js/main.js
+++ teapot/js/main.js
@@ -63,8 +63,6 @@ var Timer = 0;
 function Update() {
     for (var i = 0; i < Shapes.length; i++)
         Shapes[i].Update(0.0166667);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
     for (var i = 0; i < Shapes.length; i++)
         Shapes[i].Render();
 }
